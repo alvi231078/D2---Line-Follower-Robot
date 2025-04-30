ca0b3d9a3b6cce0ac6aaad2ca782f372e74bc954
Razin5161 <shalehinjubayer5161@gmail.com>
2025-05-01 01:16:19 +0200

    Update Line_follower_car_code_v3.ino

--- Codes/Line_follower_car_code_v3.ino
+++ Codes/Line_follower_car_code_v3.ino
@@ -1,82 +1,81 @@
-// Motor Pin Definitions
-#define motorPin1 9    // Motor 1 control pin (IN1 on L293D)
-#define motorPin2 10   // Motor 2 control pin (IN2 on L293D)
-#define motorPin3 5    // Motor 3 control pin (IN3 on L293D)
-#define motorPin4 6    // Motor 4 control pin (IN4 on L293D)
+// Motor pins
+#define motorPin1 9
+#define motorPin2 10
+#define motorPin3 5
+#define motorPin4 6
 
-// Ultrasonic Pin Definitions
-#define TRIG_PIN 7   // Trigger pin for Ultrasonic sensor
-#define ECHO_PIN 8   // Echo pin for Ultrasonic sensor
+// Ultrasonic sensor pins
+#define TRIG_PIN 7
+#define ECHO_PIN 8
 
-// IR Sensor Pin Definitions
-#define IR_LEFT 2     // Left IR sensor output
-#define IR_RIGHT 3    // Right IR sensor output
+// IR sensor pins
+#define IR_LEFT 2
+#define IR_RIGHT 3
 
 void setup() {
-  // Initialize Motor Control Pins
+  // Setup motor pins
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
 
-  // Initialize Ultrasonic Sensor Pins
+  // Setup ultrasonic pins
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 
-  // Initialize IR Sensor Pins
+  // Setup IR sensors
   pinMode(IR_LEFT, INPUT);
   pinMode(IR_RIGHT, INPUT);
 
-  Serial.begin(9600); // Debugging
+  Serial.begin(9600); // Start serial monitor
 }
 
 void loop() {
   long distance = getDistance();
 
-  // If an obstacle is close, stop
+  // Obstacle check
   if (distance < 50.8) {
     stopMotors();
     return;
   }
 
-  // Read IR Sensors
-  bool leftIR = digitalRead(IR_LEFT);   // HIGH = white, LOW = black
+  // Read IR sensors
+  bool leftIR = digitalRead(IR_LEFT);
   bool rightIR = digitalRead(IR_RIGHT);
 
-  Serial.print("Left IR: ");
+  Serial.print("L: ");
   Serial.print(leftIR);
-  Serial.print(" | Right IR: ");
+  Serial.print(" | R: ");
   Serial.println(rightIR);
 
-  // Line following logic
-  if (leftIR == LOW && rightIR == LOW) {
-    stopMotors(); // Could be intersection or dead end
+  // Line following
+  if (!leftIR && !rightIR) {
+    stopMotors();  // Both on black
   }
-  else if (leftIR == LOW && rightIR == HIGH) {
+  else if (!leftIR && rightIR) {
     turnLeft();
   }
-  else if (rightIR == LOW && leftIR == HIGH) {
+  else if (!rightIR && leftIR) {
     turnRight();
   }
   else {
-    moveForward(); // both sensors on white
+    moveForward(); // Both on white
   }
 }
 
-// Function to get distance from Ultrasonic Sensor
+// Read distance in cm
 long getDistance() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
-
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
   long duration = pulseIn(ECHO_PIN, HIGH);
-  return duration * 0.034 / 2; // in cm
+  return duration * 0.034 / 2;
 }
 
-// Movement Functions
+// Forward
 void moveForward() {
   digitalWrite(motorPin1, HIGH);
   digitalWrite(motorPin2, LOW);
@@ -84,6 +83,7 @@ void moveForward() {
   digitalWrite(motorPin4, LOW);
 }
 
+// Stop
 void stopMotors() {
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, LOW);
@@ -91,16 +91,18 @@ void stopMotors() {
   digitalWrite(motorPin4, LOW);
 }
 
+// Left turn
 void turnLeft() {
   digitalWrite(motorPin1, LOW);
-  digitalWrite(motorPin2, HIGH);  // Reverse left motor
+  digitalWrite(motorPin2, HIGH);
   digitalWrite(motorPin3, HIGH);
-  digitalWrite(motorPin4, LOW);   // Right motor forward
+  digitalWrite(motorPin4, LOW);
 }
 
+// Right turn
 void turnRight() {
   digitalWrite(motorPin1, HIGH);
-  digitalWrite(motorPin2, LOW);   // Left motor forward
+  digitalWrite(motorPin2, LOW);
   digitalWrite(motorPin3, LOW);
-  digitalWrite(motorPin4, HIGH);  // Reverse right motor
+  digitalWrite(motorPin4, HIGH);
 }
