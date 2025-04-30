5f7f8e8b177a5e34d14da942b03848d8a94ab4cc
Razin5161 <shalehinjubayer5161@gmail.com>
2025-05-01 01:12:19 +0200

    Update code_line_follwer_car.ino

--- Codes/code_line_follwer_car.ino
+++ Codes/code_line_follwer_car.ino
@@ -1,53 +1,54 @@
-// Pin definitions
-#define enA 5    // Enable Pin for Motor 1
-#define in1 6    // Motor 1 control Pin 1
-#define in2 7    // Motor 1 control Pin 2
-#define in3 9    // Motor 2 control Pin 1
-#define in4 10   // Motor 2 control Pin 2
-#define enB 8    // Enable Pin for Motor 2
-#define R_S 4    // Right IR sensor
-#define L_S 2    // Left IR sensor
+// Motor pins
+#define enA 5
+#define in1 6
+#define in2 7
+#define in3 9
+#define in4 10
+#define enB 8
+
+// Sensor pins
+#define R_S 4
+#define L_S 2
 
 void setup() {
-  // Set motor control pins as OUTPUT
+  // Motor pins as output
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
   pinMode(enB, OUTPUT);
-  
-  // Set IR sensor pins as INPUT
+
+  // Sensor pins as input
   pinMode(R_S, INPUT);
   pinMode(L_S, INPUT);
-  
-  // Activate motor drivers
+
+  // Enable motors
   digitalWrite(enA, HIGH);
   digitalWrite(enB, HIGH);
 }
 
 void loop() {
-  // Read IR sensors
-  int rightSensor = digitalRead(R_S);
-  int leftSensor = digitalRead(L_S);
-  
-  // Determine motor actions based on sensor input
-  if (rightSensor == 0 && leftSensor == 0) {
+  // Read sensors
+  int right = digitalRead(R_S);
+  int left = digitalRead(L_S);
+
+  // Decide movement
+  if (right == 0 && left == 0) {
     forward();
   } 
-  else if (rightSensor == 1 && leftSensor == 0) {
+  else if (right == 1 && left == 0) {
     turnRight();
   } 
-  else if (rightSensor == 0 && leftSensor == 1) {
+  else if (right == 0 && left == 1) {
     turnLeft();
   } 
-  else if (rightSensor == 1 && leftSensor == 1) {
+  else {
     stopMotors();
   }
 }
 
 void forward() {
-  // Move motors forward
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
@@ -55,7 +56,6 @@ void forward() {
 }
 
 void turnRight() {
-  // Turn motors to the right
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
@@ -63,7 +63,6 @@ void turnRight() {
 }
 
 void turnLeft() {
-  // Turn motors to the left
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
@@ -71,7 +70,6 @@ void turnLeft() {
 }
 
 void stopMotors() {
-  // Stop motors
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
