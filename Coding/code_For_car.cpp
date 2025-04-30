40646652761088e3173fde5444f5946c2d16e09a
Alvi <135096411+alvi231078@users.noreply.github.com>
2025-05-01 01:18:26 +0200

    updated car code uss
    updated car code to fit ultrasonic senrorr

--- Coding/code_For_car.cpp
+++ Coding/code_For_car.cpp
@@ -1,32 +1,57 @@
-const int motorPin1 = 8;   
-const int motorPin2 = 9;   
-const int enablePin = 10;  
+// Motor Pins
+const int motorPin1 = 8;
+const int motorPin2 = 9;
+const int enablePin = 10;
+
+// Ultrasonic Sensor Pins
+const int trigPin = 6;
+const int echoPin = 7;
+
+// Dc mMotor speed
+const int forwardSpeed = 180;
+const int reverseSpeed = 160;
 
 void setup() {
+  // Motor pin setup
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(enablePin, OUTPUT);
+
+  // Ultrasonic pin setup
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // motor frward
-  digitalWrite(motorPin1, HIGH);
-  digitalWrite(motorPin2, LOW);
-  analogWrite(enablePin, 180);
-  delay(2000);
+  long duration;
+  int distance;
 
-  // stop
-  analogWrite(enablePin, 0);
-  delay(1000);
+  // Triggers ultrasonic sensorr
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-  // revers dirction
-  digitalWrite(motorPin1, LOW);
-  digitalWrite(motorPin2, HIGH);
-  analogWrite(enablePin, 160);
-  delay(2000);
+  // Read echooo time and convert to distance
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2; // cm
 
-  // stop again
-  analogWrite(enablePin, 0);
-  delay(1000);
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if (distance > 15) {
+    // Forward
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    analogWrite(enablePin, forwardSpeed);
+  } else {
+    // Stop motor if obstacle detected
+    analogWrite(enablePin, 0);
+  }
+
+  delay(500); // small gives a dely befor the next loopp
 }
-// This cde is for controlling a DC motor using an Arduino. It uses PWM to control the speed and direction of the motor.
