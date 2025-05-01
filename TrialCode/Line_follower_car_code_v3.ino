e1721bfaf9179408ccfc019e0b8f9cd3a05134b8
Razin5161 <shalehinjubayer5161@gmail.com>
2025-05-01 13:05:50 +0200

    First trial code after simulating in tinkercad

--- TrialCode/Line_follower_car_code_v3.ino
+++ TrialCode/Line_follower_car_code_v3.ino
@@ -1,119 +1,78 @@
 // Motor pins
-#define motorPin1 9
-#define motorPin2 10
-#define motorPin3 5
-#define motorPin4 6
+#define enA 5
+#define in1 6
+#define in2 7
+#define in3 9
+#define in4 10
+#define enB 8
 
-// Ultrasonic sensor pins
-#define TRIG_PIN 7
-#define ECHO_PIN 8
-
-// IR sensor pins
-#define IR_LEFT 2
-#define IR_RIGHT 3
+// Sensor pins
+#define R_S 4
+#define L_S 2
 
 void setup() {
-  // Setup motor pins
-  pinMode(motorPin1, OUTPUT);
-  pinMode(motorPin2, OUTPUT);
-  pinMode(motorPin3, OUTPUT);
-  pinMode(motorPin4, OUTPUT);
+  // Motor pins as output
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(enB, OUTPUT);
 
-  // Setup ultrasonic pins
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
+  // Sensor pins as input
+  pinMode(R_S, INPUT);
+  pinMode(L_S, INPUT);
 
-  // Setup IR sensors
-  pinMode(IR_LEFT, INPUT);
-  pinMode(IR_RIGHT, INPUT);
-
-  Serial.begin(9600); // Start serial monitor
+  // Enable motors
+  digitalWrite(enA, HIGH);
+  digitalWrite(enB, HIGH);
 }
 
 void loop() {
-  long distance = getDistance();
+  // Read sensors
+  int right = digitalRead(R_S);
+  int left = digitalRead(L_S);
 
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-
-  // Check if distance is approx. 100 cm (±2 cm)
-  if (distance >= 98 && distance <= 102) {
-    Serial.println(">>> Distance is around 100 cm <<<");
-  }
-
-  // Obstacle check
-  if (distance < 50.8) {
-    stopMotors();
-    return;
-  }
-
-  // Read IR sensors
-  bool leftIR = digitalRead(IR_LEFT);
-  bool rightIR = digitalRead(IR_RIGHT);
-
-  Serial.print("L: ");
-  Serial.print(leftIR);
-  Serial.print(" | R: ");
-  Serial.println(rightIR);
-
-  // Line following
-  if (!leftIR && !rightIR) {
-    stopMotors();  // Both on black
-  }
-  else if (!leftIR && rightIR) {
-    turnLeft();
-  }
-  else if (!rightIR && leftIR) {
+  // Decide movement
+  if (right == 0 && left == 0) {
+    forward();
+  } 
+  else if (right == 1 && left == 0) {
     turnRight();
-  }
+  } 
+  else if (right == 0 && left == 1) {
+    turnLeft();
+  } 
   else {
-    moveForward(); // Both on white
+    stopMotors();
   }
-
-  delay(100); // Delay for stability
 }
 
-// Read distance in cm
-long getDistance() {
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-
-  long duration = pulseIn(ECHO_PIN, HIGH);
-  return duration * 0.034 / 2;
+void forward() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 }
 
-// Forward
-void moveForward() {
-  digitalWrite(motorPin1, HIGH);
-  digitalWrite(motorPin2, LOW);
-  digitalWrite(motorPin3, HIGH);
-  digitalWrite(motorPin4, LOW);
-}
-
-// Stop
-void stopMotors() {
-  digitalWrite(motorPin1, LOW);
-  digitalWrite(motorPin2, LOW);
-  digitalWrite(motorPin3, LOW);
-  digitalWrite(motorPin4, LOW);
-}
-
-// Left turn
-void turnLeft() {
-  digitalWrite(motorPin1, LOW);
-  digitalWrite(motorPin2, HIGH);
-  digitalWrite(motorPin3, HIGH);
-  digitalWrite(motorPin4, LOW);
-}
-
-// Right turn
 void turnRight() {
-  digitalWrite(motorPin1, HIGH);
-  digitalWrite(motorPin2, LOW);
-  digitalWrite(motorPin3, LOW);
-  digitalWrite(motorPin4, HIGH);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 }
+
+void turnLeft() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void stopMotors() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
